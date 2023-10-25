ba7e043a67dc8a15c9caf514397899ef7dfae590
Fco Padilla <a_franciscojavier.padilla.lopez@iespablopicasso.es>
2023-10-25 08:07:22 +0200

    Nuevos ficheros de prueba

---
+++ Programa.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class programa {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		
+		System.out.print("Introduce tu primer numero: ");
+		int numero1 = Integer.parseInt(sc.nextLine());
+                System.out.print("Introduce tu segundo numero: ");
+		int numero2 = Integer.parseInt(sc.nextLine());
+		
+		int suma = numero1 + numero 2;
+		
+		System.out.println("La suma de los dos numeros introducidos es " + suma);
+		
+		 
+
+	}
+
+}
